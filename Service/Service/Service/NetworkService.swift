07ca554a5d6d0a7cf8c3ec1fa9977bdb984baa46
Abhiraj <abhi266raj@gmail.com>
2023-08-05 16:55:24 +0530

    Working networking code

---
+++ Service/Service/Service/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  Service
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+import Foundation
---
+++ ViewModel/ViewModel/ViewModel/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  ViewModel
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+import Foundation
