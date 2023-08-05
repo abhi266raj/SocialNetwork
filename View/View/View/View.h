ad73bd87475aaa02211e07e4e1f8fad1dd24b926
Abhiraj <abhi266raj@gmail.com>
2023-08-05 15:07:51 +0530

    Added subproject

---
+++ View/View/View/View.h
@@ -0,0 +1,18 @@
+//
+//  View.h
+//  View
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
---
+++ ViewModel/ViewModel/ViewModel/ViewModel.h
@@ -0,0 +1,18 @@
+//
+//  ViewModel.h
+//  ViewModel
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewModel.
+FOUNDATION_EXPORT double ViewModelVersionNumber;
+
+//! Project version string for ViewModel.
+FOUNDATION_EXPORT const unsigned char ViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModel/PublicHeader.h>
+
+
