d5a6596f2ec5ca32d08583fa4d99bfdca010c836
Abhiraj <abhi266raj@gmail.com>
2023-08-05 14:53:10 +0530

    Initial Commit

---
+++ VideoViewer/VideoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoViewerApp.swift
+//  VideoViewer
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
